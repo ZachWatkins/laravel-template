656f524829633add9d9e63f17ca55b9f210ac3e7
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-02-25 14:52:14 -0600

    Add support for Laravel Horizon

---
+++ startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Install supervisor.
+apt-get install -y supervisor
+
+# Add the config directory if it does not exist.
+if [ ! -d "/etc/supervisor/conf.d" ];
+then
+    mkdir -p "/etc/supervisor/conf.d"
+fi
+
+# Copy the config file.
+cp /var/www/html/laravel-worker.conf /etc/supervisor/conf.d/laravel-worker.conf
+
+# Refresh process using configuration file.
+supervisorctl reread
+supervisorctl update
+supervisorctl start horizon
