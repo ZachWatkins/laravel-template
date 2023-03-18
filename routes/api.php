6ad412ddcb5ac2343e0847a3f5f8ff4241c13188
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-03-18 15:59:20 -0500

    Add Laravel Breeze for authentication

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\LocationController;
 use App\Http\Controllers\ImportController;
 use App\Http\Controllers\ExportController;
 use App\Http\Controllers\DownloadController;
-use Illuminate\Routing\Middleware\ValidateSignature;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,12 +13,12 @@ use Illuminate\Routing\Middleware\ValidateSignature;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
