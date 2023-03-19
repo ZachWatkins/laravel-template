583a4dc5feed5dc70e78b6a1d8ff04783a7e492f
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-03-18 22:08:19 -0500

    Reverse all changes made when adding Laravel Breeze

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\LocationController;
 use App\Http\Controllers\ImportController;
 use App\Http\Controllers\ExportController;
 use App\Http\Controllers\DownloadController;
+use Illuminate\Routing\Middleware\ValidateSignature;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,12 +14,12 @@ use App\Http\Controllers\DownloadController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
