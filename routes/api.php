af2ba80c1951b83592bf013195a430fe1b5cf45f
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-03-19 14:23:47 -0500

    Revert "Install Laravel Breeze for APIs"
    This reverts commit 724cba6f0d71789e078e0d91525b48a531249df9.

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LocationController;
+use App\Http\Controllers\ImportController;
+use App\Http\Controllers\ExportController;
+use App\Http\Controllers\DownloadController;
+use Illuminate\Routing\Middleware\ValidateSignature;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,11 +14,18 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('locations', LocationController::class);
+Route::post('/import', ImportController::class);
+Route::get('/export', [ExportController::class, 'index']);
+Route::get('/export/create', [ExportController::class, 'create']);
+Route::get('/download', DownloadController::class)
+    ->name('download')->middleware('signed');
