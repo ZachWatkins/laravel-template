ab4daeb8de2ad82651d08b9513e04c28796a31ae
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-03-19 23:45:32 -0500

    Update user information route

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Routing\Middleware\ValidateSignature;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/api/user/me', function (Request $request) {
     return $request->user();
 });
 
