db6206163e2f2fb70bef59ea324525d6f19f376a
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-03-30 22:27:43 -0500

    Resolve infinite loop error

--- app/Providers/UserStorageServiceProvider.php
+++ app/Providers/UserStorageServiceProvider.php
@@ -4,27 +4,18 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Contracts\Filesystem\Filesystem;
 
-class UserStorageServiceProvider extends ServiceProvider
+class UserStorageServiceProvider extends ServiceProvider implements DeferrableProvider
 {
     /**
      * Register services.
      */
     public function register(): void
     {
-        $this->app->scoped('auth', function ($app) {
-            $userId = auth()->id();
-            $rootPath = "users/{$userId}";
-            return Storage::createLocalDriver(['root' => $rootPath]);
-        });
-
-        $this->app->scoped('auth-public', function ($app) {
-            $userId = auth()->id();
-            $config = config('filesystems.disks.public');
-            $config['root'] .= "/users/{$userId}";
-            $config['url'] .= "/users/{$userId}";
-            return Storage::createLocalDriver($config);
-        });
+        //
     }
 
     /**
@@ -32,15 +23,32 @@ class UserStorageServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        fwrite(STDERR, 'UserStorageServiceProvider::boot()' . PHP_EOL);
         Storage::extend('user', function ($app, $config) {
-            $config['root'] = str_replace('{user_id}', auth()->id(), $config['root']);
+            fwrite(STDERR, Auth::user()->id . PHP_EOL);
+            fwrite(STDERR, Auth::id() . PHP_EOL);
+            $config = config('filesystems.disks.user');
+            $config['root'] = str_replace('{user_id}', Auth::user()->id, $config['root']);
             return Storage::createLocalDriver($config);
         });
         Storage::extend('user-public', function ($app, $config) {
-            $user_id = auth()->id();
+            fwrite(STDERR, Auth::user()->id . PHP_EOL);
+            fwrite(STDERR, Auth::id() . PHP_EOL);
+            $user_id = Auth::user()->id;
+            $config = config('filesystems.disks.user-public');
             $config['root'] = str_replace('{user_id}', $user_id, $config['root']);
             $config['url'] = str_replace('{user_id}', $user_id, $config['url']);
             return Storage::createLocalDriver($config);
         });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array<int, string>
+     */
+    public function provides(): array
+    {
+       return [Filesystem::class];
     }
 }
