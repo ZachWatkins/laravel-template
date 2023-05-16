64a39384542a4a3d307a05b94ba8bb3c99d34f4f
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-05-15 22:13:12 -0500

    Add cron scheduled task runner

--- startup.sh
+++ startup.sh
@@ -15,3 +15,16 @@ cp /var/www/html/laravel-worker.conf /etc/supervisor/conf.d/laravel-worker.conf
 supervisorctl reread
 supervisorctl update
 supervisorctl start laravel-worker:*
+
+# Initialize scheduled Laravel commands.
+service cron stop
+apt-get update -y
+apt-get install -y cron
+COMMAND="* * * * * cd /var/www/html && php artisan schedule:run >> /dev/null 2>&1"
+crontab -l | grep "$COMMAND" 1>/dev/null 2>&1
+(( $? == 0 )) && exit
+crontab -l >/tmp/crontab.tmp
+echo "$COMMAND" >> /tmp/crontab.tmp
+crontab /tmp/crontab.tmp
+rm /tmp/crontab.tmp
+service cron start
