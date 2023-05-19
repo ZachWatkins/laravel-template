a931a7816f9be8e8c5c928723c084993eff94447
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-05-18 22:09:30 -0500

    Update user event subscriber to check whether an event should be created first

--- app/Listeners/UserEventSubscriber.php
+++ app/Listeners/UserEventSubscriber.php
@@ -25,125 +25,161 @@ class UserEventSubscriber
      * Handle user registered events.
      */
     public function handleUserRegistered(Registered $event): void {
+
         $user = User::find((int) $event->user->getAuthIdentifier());
+
         if (!$user) {
             throw new \Exception('User not found');
         }
+
         if ($user->registered_at) {
             throw new \Exception('User already registered');
         }
+
         $user->registered_at = now();
         $user->save();
+
+        AuthEvent::create([
+            'user_id' => (int) $event->user->getAuthIdentifier(),
+            'action' => 'registered',
+        ]);
+
     }
 
     /**
      * Handle user attempting events.
      */
     public function handleUserAttempting(Attempting $event): void {
+
+        if (!User::where('email', $event->credentials['email'])->exists()) {
+            return;
+        }
+
         AuthEvent::create([
             'action' => 'attempting',
             'payload' => $event->credentials['email'],
         ]);
+
     }
 
     /**
      * Handle user authenticated events.
      */
     public function handleUserAuthenticated(Authenticated $event): void {
+
         AuthEvent::create([
             'user_id' => $event->user->getAuthIdentifier(),
             'action' => 'authenticated',
         ]);
+
     }
 
     /**
      * Handle user login events.
      */
     public function handleUserLogin(Login $event): void {
+
         AuthEvent::create([
             'user_id' => $event->user->getAuthIdentifier(),
             'action' => 'login',
         ]);
+
     }
 
     /**
      * Handle user failed events.
      */
     public function handleUserFailed(Failed $event): void {
+
         AuthEvent::create([
             'action' => 'failed',
             'payload' => $event->credentials['email'],
         ]);
+
     }
 
     /**
      * Handle user validated events.
      */
     public function handleUserValidated(Validated $event): void {
+
         AuthEvent::create([
             'user_id' => $event->user->getAuthIdentifier(),
             'action' => 'validated',
         ]);
+
     }
 
     /**
      * Handle user verified events.
      */
     public function handleUserVerified(Verified $event): void {
+
         AuthEvent::create([
             'action' => 'verified',
             'payload' => $event->user->getEmailForVerification(),
         ]);
+
     }
 
     /**
      * Handle user logout events.
      */
     public function handleUserLogout(Logout $event): void {
+
         AuthEvent::create([
             'user_id' => $event->user->getAuthIdentifier(),
             'action' => 'logout',
         ]);
+
     }
 
     /**
      * Handle user current device logout events.
      */
     public function handleUserCurrentDeviceLogout(CurrentDeviceLogout $event): void {
+
         AuthEvent::create([
             'user_id' => $event->user->getAuthIdentifier(),
             'action' => 'current_device_logout',
         ]);
+
     }
 
     /**
      * Handle user other device logout events.
      */
     public function handleUserOtherDeviceLogout(OtherDeviceLogout $event): void {
+
         AuthEvent::create([
             'user_id' => $event->user->getAuthIdentifier(),
             'action' => 'other_device_logout',
         ]);
+
     }
 
     /**
      * Handle user lockout events.
      */
     public function handleUserLockout(Lockout $event): void {
+
         AuthEvent::create([
             'action' => 'lockout',
             'payload' => $event->request->email,
         ]);
+
     }
 
     /**
      * Handle user password reset events.
      */
     public function handlePasswordReset(PasswordReset $event): void {
+
         AuthEvent::create([
             'user_id' => $event->user->getAuthIdentifier(),
             'action' => 'password_reset',
         ]);
+
     }
 
     /**
