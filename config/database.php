3e5fc8dce6ba3b929340c2ff33d9e6bb087c8d80
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-07-01 14:39:13 -0500

    Configure test database as a sqlite file

--- config/database.php
+++ config/database.php
@@ -95,8 +95,10 @@ return [
 
         'sqlite_testing' => [
             'driver' => 'sqlite',
-            'database' => ':memory:',
+            'url' => env('DATABASE_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
     ],
