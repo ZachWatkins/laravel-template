f611743b8991d880928be2946ab9108896cf1135
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2024-09-23 20:57:44 -0500

    Add post links to main dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,9 +7,14 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden p-6 shadow-sm sm:rounded-lg">
+                <div class="mb-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
+                </div>
+                <h1 class="text-2xl font-semibold inline">{{ __("Posts") }}</h1>
+                <div class="mb-6 text-gray-900 dark:text-gray-100">
+                    <a href="{{ route('posts.index') }}" class="underline text-blue-500 hover:text-blue-700">{{ __("View Posts") }}</a><br />
+                    <a href="{{ route('posts.create') }}" class="underline text-blue-500 hover:text-blue-700">{{ __("Create Post") }}</a>
                 </div>
             </div>
         </div>
