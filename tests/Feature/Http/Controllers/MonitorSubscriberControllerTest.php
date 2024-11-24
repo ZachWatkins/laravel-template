e2e9d7fa7d1f60b89efb54b8a38f9168802bc70f
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2024-11-24 14:02:44 -0600

    Apply custom test assertion class

--- tests/Feature/Http/Controllers/MonitorSubscriberControllerTest.php
+++ tests/Feature/Http/Controllers/MonitorSubscriberControllerTest.php
@@ -11,6 +11,8 @@ use function Pest\Laravel\get;
 use function Pest\Laravel\post;
 use function Pest\Laravel\put;
 
+uses(\JMac\Testing\Traits\AdditionalAssertions::class);
+
 test('index displays view', function (): void {
     $monitorSubscribers = MonitorSubscriber::factory()->count(3)->create();
 
