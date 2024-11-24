ae466a2ab89a1a471d9737ac6ce2d54c5a2cee7a
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2024-11-24 16:41:22 -0600

    Fill in enum attributes

--- tests/Feature/Http/Controllers/PartWebpageControllerTest.php
+++ tests/Feature/Http/Controllers/PartWebpageControllerTest.php
@@ -40,7 +40,7 @@ test('store uses form request validation')
     );
 
 test('store saves and redirects', function (): void {
-    $status = fake()->randomElement(/** enum_attributes **/);
+    $status = fake()->randomElement(['active', 'inactive']);
     $path = fake()->word();
     $title = fake()->sentence(4);
     $meta_title = fake()->word();
@@ -110,7 +110,7 @@ test('update uses form request validation')
 
 test('update redirects', function (): void {
     $partWebpage = PartWebpage::factory()->create();
-    $status = fake()->randomElement(/** enum_attributes **/);
+    $status = fake()->randomElement(['active', 'inactive']);
     $path = fake()->word();
     $title = fake()->sentence(4);
     $meta_title = fake()->word();
