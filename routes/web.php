c2895a9313f495036b0f1a2c22ef032bb4795956
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2024-11-28 09:44:25 -0600

    Remove previously defined routes (#32)

--- routes/web.php
+++ routes/web.php
@@ -35,13 +35,7 @@ Route::resource('customers', App\Http\Controllers\CustomerController::class);
 
 Route::resource('customer-payment-methods', App\Http\Controllers\CustomerPaymentMethodController::class);
 
-Route::get('shopping-carts/add-item', [App\Http\Controllers\ShoppingCartController::class, 'addItem']);
-Route::get('shopping-carts/remove-item', [App\Http\Controllers\ShoppingCartController::class, 'removeItem']);
-Route::get('shopping-carts/empty', [App\Http\Controllers\ShoppingCartController::class, 'empty']);
-Route::get('shopping-carts/order', [App\Http\Controllers\ShoppingCartController::class, 'order']);
-
 Route::resource('orders', App\Http\Controllers\OrderController::class)->only('index', 'store', 'show');
-
 
 Route::resource('shopping-carts', App\Http\Controllers\ShoppingCartController::class);
 
